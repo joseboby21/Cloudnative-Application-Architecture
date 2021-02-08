4d3a8d4fcbb15dfd0fae3fa36de1db2fbe7a0c0b
Boby <joseboby21@gmail.com>
2021-02-08 17:38:27 -0500

    Lab3 completed

---
+++ Lab3/chat/chat.go
@@ -0,0 +1,105 @@
+// Demonstration of channels with a chat application
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// Chat is a server that lets clients chat with each other.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+type client struct{
+	name string
+	chl  chan<- string // an outgoing message channel
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func activeUsers(clients map[client]bool, ch chan<- string){
+	if len(clients)<1{
+		return
+	}
+	ch <- "Current active users are"
+	for cl := range clients{
+		ch <- cl.name
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			name := strings.Fields(msg)
+			for cli := range clients {
+				if cli.name+":" != name[0]{
+					cli.chl <- msg
+				}
+			}
+
+		case cli := <-entering:
+			activeUsers(clients,cli.chl)
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.chl)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string) // outgoing client messages
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	usr := client{name:who,chl:ch}
+	entering <- usr
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+	// NOTE: ignoring potential errors from input.Err()
+
+	leaving <- usr
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg) // NOTE: ignoring network errors
+	}
+}
