87cd85c86ea260821c8ad97ca78feb4786c91661
Boby <joseboby21@gmail.com>
2021-02-25 09:51:46 -0500

    Lab4 Completed

--- Lab4/webserver.go
+++ Lab4/webserver.go
@@ -8,8 +8,16 @@ import (
 	"sync"
 )
 
+type dollars float32
+type database struct {
+	DB map[string]dollars
+	RW sync.RWMutex
+}
+
 func main() {
-	db := database{"shoes": 50, "socks": 5}
+	db := database{DB: make(map[string]dollars)}
+	db.DB["shoes"] = dollars(50)
+	db.DB["sock"] = dollars(5)
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/price", db.price)
 	http.HandleFunc("/add", db.add)
@@ -18,85 +26,76 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-type dollars float32
-
 func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
-type database map[string]dollars
-
-//RW : Read Write lock for DB
-var RW sync.RWMutex
-
-func (db database) list(w http.ResponseWriter, req *http.Request) {
-	RW.RLock()
-	for item, price := range db {
+func (db *database) list(w http.ResponseWriter, req *http.Request) {
+	db.RW.RLock()
+	defer db.RW.RUnlock()
+	for item, price := range db.DB {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
-	RW.RUnlock()
+
 }
 
-func (db database) price(w http.ResponseWriter, req *http.Request) {
-	RW.RLock()
+func (db *database) price(w http.ResponseWriter, req *http.Request) {
+	db.RW.RLock()
+	defer db.RW.RUnlock()
 	item := req.URL.Query().Get("item")
-	if price, ok := db[item]; ok {
+	if price, ok := db.DB[item]; ok {
 		fmt.Fprintf(w, "%s\n", price)
 	} else {
-		w.WriteHeader(http.StatusNotFound) // 404
+		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "no such item: %q\n", item)
 	}
-	RW.RUnlock()
 }
 
-func (db database) add(w http.ResponseWriter, req *http.Request) {
-	RW.Lock()
+func (db *database) add(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if _, ok := db[item]; ok {
+	if _, ok := db.DB[item]; ok {
 		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "Item Already Present\n")
-		RW.Unlock()
 		return
 	}
 	price := req.URL.Query().Get("price")
 	if s, err := strconv.ParseFloat(price, 32); err == nil {
-		db[item] = dollars(s)
+		db.RW.Lock()
+		db.DB[item] = dollars(s)
+		db.RW.Unlock()
 		fmt.Fprintf(w, "%s added to list\n", item)
 	} else {
 		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "Not a Valid Price\n")
 	}
-	RW.Unlock()
 }
 
-func (db database) update(w http.ResponseWriter, req *http.Request) {
-	RW.Lock()
+func (db *database) update(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if _, ok := db[item]; !ok {
-		w.WriteHeader(http.StatusBadRequest) // 404
+	if _, ok := db.DB[item]; !ok {
+		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "no such item: %q\n", item)
-		RW.Unlock()
 		return
 	}
 	price := req.URL.Query().Get("price")
 	if s, err := strconv.ParseFloat(price, 32); err == nil {
-		db[item] = dollars(s)
+		db.RW.Lock()
+		db.DB[item] = dollars(s)
+		db.RW.Unlock()
 		fmt.Fprintf(w, "%s price updated\n", item)
 	} else {
 		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "Not a Valid Price\n")
 	}
-	RW.Unlock()
 }
 
-func (db database) delete(w http.ResponseWriter, req *http.Request) {
-	RW.Lock()
+func (db *database) delete(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if _, ok := db[item]; !ok {
-		w.WriteHeader(http.StatusBadRequest) // 404
+	if _, ok := db.DB[item]; !ok {
+		w.WriteHeader(http.StatusBadRequest) // 400
 		fmt.Fprintf(w, "no such item: %q\n", item)
-		RW.Unlock()
 		return
 	}
-	delete(db, item)
+	db.RW.Lock()
+	delete(db.DB, item)
+	db.RW.Unlock()
 	fmt.Fprintf(w, "%q item deleted from list\n", item)
-	RW.Unlock()
 }
